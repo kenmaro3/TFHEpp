afe79ee44a32937a9f94c3b30f84f5184a13b925
kenmaro3 <miha.ken.19@gmail.com>
2021-07-28 03:07:24 +0000

    [ADD] naive omp parallel

--- tutorial/mytest3.cpp
+++ tutorial/mytest3.cpp
@@ -12,6 +12,8 @@
 #include <cmath>
 #include <string>
 
+#include <omp.h>
+
 using namespace std;
 using namespace TFHEpp;
 
@@ -90,10 +92,37 @@ vector<TLWE<P>> encrypt_vector(const vector<double> xs, const array<typename P::
 }
 
 template <class P>
+vector<TLWE<P>> encrypt_vector_omp(const vector<double> xs, const array<typename P::T, P::n> sk, Encoder &encoder){
+  vector<TLWE<P>> res(xs.size());
+  int i;
+  #pragma omp parallel for private(i)
+  for(i=0; i<xs.size(); i++){
+    res[i] = TFHEpp::tlweSymEncodeEncrypt<P>(xs[i], P::alpha, sk, encoder);
+  }
+  return res;
+}
+
+template <class P>
 vector<double> decrypt_vector(const vector<TLWE<P>> cs, const array<typename P::T, P::n> sk, Encoder &encoder, bool is_print=true){
   vector<double> res;
   for(int i=0; i<cs.size(); i++){
     res.push_back(TFHEpp::tlweSymDecryptDecode<P>(cs[i], sk, encoder));
+  }
+
+  if(is_print){
+    print_vec_1d(res);
+  }
+  return res;
+}
+
+template <class P>
+vector<double> decrypt_vector_omp(const vector<TLWE<P>> cs, const array<typename P::T, P::n> sk, Encoder &encoder, bool is_print=true){
+  vector<double> res(cs.size());
+
+  int i;
+  #pragma omp parallel for private(i)
+  for(i=0; i<cs.size(); i++){
+    res[i] = TFHEpp::tlweSymDecryptDecode<P>(cs[i], sk, encoder);
   }
 
   if(is_print){
@@ -186,6 +215,23 @@ vector<TLWE<p>> relu(const vector<TLWE<p>> cs, const GateKey &gk, Encoder &encod
   return res;
 }
 
+template <class p>
+vector<TLWE<p>> relu_omp(const vector<TLWE<p>> cs, const GateKey &gk, Encoder &encoder, double expansion=1.){
+  Encoder encoder_domain = Encoder::copy(encoder);
+  Encoder encoder_target = Encoder::copy(encoder);
+  vector<TLWE<p>> res(cs.size());
+
+  int i;
+  #pragma omp parallel for private(i)
+  for(i=0; i<cs.size(); i++){
+    //encoder_domain.print();
+    TFHEpp::ProgrammableBootstrapping(res[i], cs[i], gk, encoder_domain, encoder_target, my_relu_function); 
+  }
+
+  encoder = encoder_target;
+  return res;
+}
+
 vector<double> relu(const vector<double> x, bool is_print=true){
   vector<double> res;
   for(int i=0; i<x.size(); i++){
@@ -208,6 +254,23 @@ vector<TLWE<p>> sigmoid(const vector<TLWE<p>> cs, const GateKey &gk, Encoder &en
   vector<TLWE<p>> res(cs.size());
 
   for(int i=0; i<cs.size(); i++){
+    //encoder_domain.print();
+    TFHEpp::ProgrammableBootstrapping(res[i], cs[i], gk, encoder_domain, encoder_target, my_sigmoid_function); 
+  }
+
+  encoder = encoder_target;
+  return res;
+}
+
+template <class p>
+vector<TLWE<p>> sigmoid_omp(const vector<TLWE<p>> cs, const GateKey &gk, Encoder &encoder, double expansion=1.){
+  Encoder encoder_domain = Encoder::copy(encoder);
+  Encoder encoder_target = Encoder(-2, 2, encoder.bp);
+  vector<TLWE<p>> res(cs.size());
+
+  int i;
+  #pragma omp parallel for private(i)
+  for(i=0; i<cs.size(); i++){
     //encoder_domain.print();
     TFHEpp::ProgrammableBootstrapping(res[i], cs[i], gk, encoder_domain, encoder_target, my_sigmoid_function); 
   }
@@ -275,6 +338,35 @@ vector<TLWE<P>> wtx(const vector<TLWE<P>> cs, const vector<vector<double>> w, co
   return res;
 }
 
+template <class P>
+vector<TLWE<P>> wtx_omp(const vector<TLWE<P>> cs, const vector<vector<double>> w, const GateKey &gk, Encoder &encoder, double expansion){
+  assert(cs.size() == w[0].size());
+
+  Encoder encoder_domain = Encoder::copy(encoder);
+  Encoder encoder_target = Encoder::copy(encoder);
+  double max_ds = 0.;
+  for(int i=0; i<w.size(); i++){
+    max_ds = max(max_ds, get_max(w[i]));
+  }
+  max_ds = get_wider(max_ds);
+  max_ds *= expansion;
+  //printf("max_ds: %f\n", max_ds);
+  encoder_target.update(max_ds);
+  //encoder_target.print();
+
+  vector<TLWE<P>> res(w.size());
+
+  int i;
+  #pragma omp parallel for private(i)
+  for(i=0; i<w.size(); i++){
+    res[i] = inner<P>(cs, w[i], gk, encoder_domain, encoder_target);
+  }
+
+  encoder = encoder_target;
+
+  return res;
+}
+
 vector<double> wtx(const vector<double> x, const vector<vector<double>> w, bool is_print=true){
   assert (x.size() == w[0].size());
   vector<double> res;
@@ -296,6 +388,19 @@ vector<TLWE<P>> btx(const vector<TLWE<P>> cs, const vector<double> b, Encoder &e
     TLWE<P> tmp;
     TFHEpp::HomADDCONST(tmp, cs[i], b[i], encoder);
     res.push_back(tmp);
+  }
+  return res;
+}
+
+template <class P>
+vector<TLWE<P>> btx_omp(const vector<TLWE<P>> cs, const vector<double> b, Encoder &encoder){
+  assert(cs.size() == b.size());
+
+  vector<TLWE<P>> res(b.size());
+  int i;
+  #pragma omp parallel for private(i)
+  for(i=0; i<b.size(); i++){
+    TFHEpp::HomADDCONST(res[i], cs[i], b[i], encoder);
   }
   return res;
 }
@@ -334,12 +439,35 @@ vector<TLWE<P>> linear(const vector<TLWE<P>> cs, const vector<vector<double>> w,
 }
 
 template <class P>
+vector<TLWE<P>> linear_omp(const vector<TLWE<P>> cs, const vector<vector<double>> w, const vector<double> b, const GateKey &gk, Encoder &encoder){
+  vector<TLWE<lvl0param>> c2 = wtx_omp<lvl0param>(cs, w, gk, encoder, 2.);
+  printf("multed\n");
+  vector<TLWE<lvl0param>> c3 = btx_omp<lvl0param>(c2, b, encoder);
+  printf("added\n");
+  return c3;
+}
+
+template <class P>
 vector<TLWE<P>> nonlinear(const vector<TLWE<P>> cs, const GateKey gk, Encoder &encoder, string nonlinear_type){
   if(nonlinear_type=="sigmoid"){
     vector<TLWE<lvl0param>> c2 = sigmoid<lvl0param>(cs, gk, encoder);
     return c2;
   }else if(nonlinear_type=="relu"){
     vector<TLWE<lvl0param>> c2 = relu<lvl0param>(cs, gk, encoder);
+    return c2;
+  }else{
+    cerr << "not known nl_type" << endl;
+  }
+
+}
+
+template <class P>
+vector<TLWE<P>> nonlinear_omp(const vector<TLWE<P>> cs, const GateKey gk, Encoder &encoder, string nonlinear_type){
+  if(nonlinear_type=="sigmoid"){
+    vector<TLWE<lvl0param>> c2 = sigmoid_omp<lvl0param>(cs, gk, encoder);
+    return c2;
+  }else if(nonlinear_type=="relu"){
+    vector<TLWE<lvl0param>> c2 = relu_omp<lvl0param>(cs, gk, encoder);
     return c2;
   }else{
     cerr << "not known nl_type" << endl;
@@ -374,7 +502,7 @@ double avg(vector<double> x){
   return res/double(x.size());
 }
 
-int main(){
+void test(){
   printf("hello, world\n\n");
 
   // ###########################################
@@ -473,5 +601,113 @@ int main(){
   printf("time avg: %f\n", ts_avg);
 
   
+
+}
+
+void test_omp(){
+  printf("hello, world\n\n");
+
+  // ###########################################
+  // model side parameters
+  printf("\nprepare_model=============================================================\n");
+  int d1 = 5;
+  int d2 = 3;
+  int d3 = 1;
+  string nl_type1 = "sigmoid";
+  string nl_type2 = "sigmoid";
+  vector<double> d;
+
+  vector<double> x1 = give_me_vector(d1);
+  vector<vector<double>> w = give_me_weight(d2, d1);
+  vector<double> b = give_me_vector(d2);
+  vector<vector<double>> w2 = give_me_weight(d3, d2);
+  vector<double> b2 = give_me_vector(d3);
+
+  printf("\n=============================================================\n");
+  printf("x1\n");
+  print_vec_1d(x1);
+  printf("w\n");
+  print_vec_2d(w);
+  printf("b\n");
+  print_vec_1d(b);
+
+  printf("\n=============================================================\n");
+  printf("w\n");
+  print_vec_2d(w2);
+  printf("b\n");
+  print_vec_1d(b2);
+
+
+  // ###########################################
+  // encoder side parameters
+  printf("\n=============================================================\n");
+  double encoder_a = -3.;
+  double encoder_b = 3.;
+  int bs_bp = 32;
+
+  TFHEpp::Encoder encoder(encoder_a, encoder_b, bs_bp);
+  printf("\nparepare encoder=============================================================\n");
+
+  // generate a random key
+  std::unique_ptr<TFHEpp::SecretKey> sk =
+      std::make_unique<TFHEpp::SecretKey>();
+  std::unique_ptr<TFHEpp::GateKey> gk =
+      std::make_unique<TFHEpp::GateKey>(*sk, encoder);
+
+  printf("\nencryption=============================================================\n");
+  vector<TLWE<lvl0param>> c1 = encrypt_vector_omp<lvl0param>(x1, sk->key.lvl0, encoder);
+  decrypt_vector_omp<lvl0param>(c1, sk->key.lvl0, encoder);
+
+
+  double start, end;
+  vector<double> ts;
+  bool is_decrypt = false;
+  for(int i=0; i<3; i++){
+    start = get_time_msec();
+
+    printf("\ncipher=============================================================\n");
+    vector<TLWE<lvl0param>> a1 = linear_omp<lvl0param>(c1, w, b, *gk.get(), encoder);
+    if(is_decrypt)
+      printf("\nlineared \n");
+      decrypt_vector<lvl0param>(a1, sk->key.lvl0, encoder);
+    //vector<TLWE<lvl0param>> y1 = nonlinear<lvl0param>(a1, *gk.get(), encoder, nl_type1);
+    printf("\nnon-lineared\n");
+    vector<TLWE<lvl0param>> y1 = sigmoid_omp<lvl0param>(a1, *gk.get(), encoder);
+    if(is_decrypt)
+      decrypt_vector<lvl0param>(y1, sk->key.lvl0, encoder);
+
+    printf("\nraw_debug=============================================================\n");
+    vector<double> tmp = linear(x1, w, b);
+    tmp = nonlinear(tmp, nl_type1);
+
+    printf("\ncipher=============================================================\n");
+    vector<TLWE<lvl0param>> a2 = linear_omp<lvl0param>(y1, w2, b2, *gk.get(), encoder);
+    printf("\nlineared\n");
+    if(is_decrypt)
+      decrypt_vector<lvl0param>(a2, sk->key.lvl0, encoder);
+    //vector<TLWE<lvl0param>> y2 = nonlinear<lvl0param>(a2, *gk.get(), encoder, nl_type2);
+    vector<TLWE<lvl0param>> y2 = sigmoid_omp<lvl0param>(a2, *gk.get(), encoder);
+    printf("\nnon-lineared\n");
+    if(is_decrypt)
+      decrypt_vector<lvl0param>(y2, sk->key.lvl0, encoder);
+
+    printf("\nraw_debug=============================================================\n");
+    tmp = linear(tmp, w2, b2);
+    tmp = nonlinear(tmp, nl_type2);
+
+    end = get_time_msec();
+    ts.push_back(end-start);
+  }
+
+  double ts_avg = avg(ts);
+  printf("time avg: %f\n", ts_avg);
+
+  
+
+}
+
+int main(){
+  //test();
+  test_omp();
   return 0;
 }
