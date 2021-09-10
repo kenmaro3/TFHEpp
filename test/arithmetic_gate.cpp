d79aa685303beef8367370f2123ee35b127cc0ad
akakou <akakou571@gmail.com>
2021-09-10 20:03:47 +0900

    [ADD] fixed and const adder

--- test/arithmetic_gate.cpp
+++ test/arithmetic_gate.cpp
@@ -6,12 +6,17 @@
 using namespace std;
 using namespace TFHEpp;
 
-class AddTester : public AbstructBootstrapTester {
+#define ADD_ARG                                                     \
+    TFHEpp::TLWE<lvl0param> &c3, const TFHEpp::TLWE<lvl0param> &c1, \
+        const TFHEpp::TLWE<lvl0param> &c2, Encoder &encoder_domain, \
+        Encoder &encoder_target, const double x1, const double x2
+
+class AbstructAddTester : public AbstructBootstrapTester {
 public:
     int dist_max = 100;
     double permit_error = dist_max / 100;
 
-    AddTester(random_device &seed_gen)
+    AbstructAddTester(random_device &seed_gen)
     {
         init(function, seed_gen, dist_max, permit_error);
     }
@@ -23,22 +28,49 @@ public:
         double x1 = dist(engine);
         double x2 = dist(engine);
 
-        TFHEpp::Encoder encoder(-dist_max, dist_max, 32);
+        TFHEpp::Encoder encoder(-dist_max * 2, dist_max * 2, 32);
 
         c1 = TFHEpp::tlweSymEncodeEncrypt<TFHEpp::lvl0param>(
             x1, TFHEpp::lvl0param::alpha, sk->key.lvl0, encoder);
         c2 = TFHEpp::tlweSymEncodeEncrypt<TFHEpp::lvl0param>(
             x2, TFHEpp::lvl0param::alpha, sk->key.lvl0, encoder);
 
-        TFHEpp::HomADD(c3, c1, c2, encoder, encoder);
+        add(c3, c1, c2, encoder, encoder, x1, x2);
 
         double d = TFHEpp::tlweSymDecryptDecode<TFHEpp::lvl0param>(
             c3, sk->key.lvl0, encoder);
 
-        // printf("%lf + %lf = %lf ~= %lf\n\n", x1, x2, x1 + x2, d);
-
         return assert_test(x1 + x2, d, false);
     }
+
+    virtual void add(ADD_ARG) = 0;
+};
+
+class AddTester : public AbstructAddTester {
+public:
+    AddTester(random_device &seed_gen) : AbstructAddTester(seed_gen) {}
+
+    void add(ADD_ARG)
+    {
+        TFHEpp::HomADD(c3, c1, c2, encoder_domain, encoder_target);
+    };
+};
+
+class AddFixedEncoder : public AbstructAddTester {
+public:
+    AddFixedEncoder(random_device &seed_gen) : AbstructAddTester(seed_gen) {}
+
+    void add(ADD_ARG)
+    {
+        TFHEpp::HomADDFixedEncoder(c3, c1, c2, encoder_domain, encoder_target);
+    };
+};
+
+class AddConst : public AbstructAddTester {
+public:
+    AddConst(random_device &seed_gen) : AbstructAddTester(seed_gen) {}
+
+    void add(ADD_ARG) { TFHEpp::HomADDCONST(c3, c1, x2, encoder_domain); };
 };
 
 class MulTester : public AbstructBootstrapTester {
@@ -62,7 +94,6 @@ public:
         x2 = abs(x2);
 
         TFHEpp::Encoder encoder_domain(-40, 40, 32);
-
         TFHEpp::Encoder encoder_target(-40, 1800, 32);
 
         c1 = TFHEpp::tlweSymEncodeEncrypt<TFHEpp::lvl0param>(
@@ -76,7 +107,8 @@ public:
         double d = TFHEpp::tlweSymDecryptDecode<TFHEpp::lvl0param>(
             c3, sk->key.lvl0, encoder_target);
 
-        // printf("%lf x %lf = %lf ~= %lf\n\n", x1, x2, x1 * x2, d);
+        // printf("%lf x %lf = %lf ~= %lf\n\n", x1, x2, x1
+        // * x2, d);
 
         return assert_test(abs(x1 * x2), d, false);
     }
@@ -86,10 +118,13 @@ int main()
 {
     random_device seed_gen;
 
-    auto add_tester = AddTester(seed_gen);
     auto mul_tester = MulTester(seed_gen);
+    auto add_tester = AddTester(seed_gen);
+    auto add_fixed_tester = AddFixedEncoder(seed_gen);
+    auto add_const = AddConst(seed_gen);
 
-    std::vector<AbstructBootstrapTester *> testers{&add_tester, &mul_tester};
+    std::vector<AbstructBootstrapTester *> testers{
+        &add_tester, &add_fixed_tester, &add_const, &mul_tester};
 
     test(testers);
 }
